9c86f83975e7d50f05c575b427d6021caf1874ed
Cecchy2 <dariocecchinato@gmail.com>
2024-06-12 15:38:46 +0200

    finito esercizio

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//https://striveschool-api.herokuapp.com/books
-
 const fetchBooks = () => {
   fetch("https://striveschool-api.herokuapp.com/books")
     .then((responseObj) => {
@@ -12,6 +10,42 @@ const fetchBooks = () => {
     })
     .then((booksObj) => {
       console.log("booksObj", booksObj);
+
+      booksObj.forEach((libro) => {
+        const row = document.getElementById("rowCards");
+        const col = document.createElement("div");
+        col.classList.add("col-3");
+        const card = document.createElement("div");
+        card.classList.add("card");
+        const img = document.createElement("img");
+        img.classList.add("card-img-top");
+        img.src = libro.img;
+        const titolo = document.createElement("h5");
+        titolo.classList.add("card-title");
+        titolo.innerText = libro.title;
+        const price = document.createElement("p");
+        price.classList.add("card-text");
+        price.innerText = libro.price + "$";
+        const scartaBtn = document.createElement("button");
+        scartaBtn.classList.add("btn");
+        scartaBtn.classList.add("btn-primary");
+        scartaBtn.innerHTML = "scarta";
+        scartaBtn.addEventListener("click", () => {
+          col.remove();
+        });
+
+        card.appendChild(img);
+        card.appendChild(titolo);
+        card.appendChild(price);
+        card.appendChild(scartaBtn);
+
+        col.appendChild(card);
+
+        row.appendChild(col);
+      });
+    })
+    .catch((error) => {
+      console.error("FETCH ERROR: ", error);
     });
 };
 
